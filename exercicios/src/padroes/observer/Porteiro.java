28ea891cc93792caa4720f60bf7643d0a35867b2
Jhuan <juanbarrosdev@gmail.com>
2024-07-26 22:31:45 -0300

    Dando os meus primeiros passos com a API Swing.

---
+++ exercicios/src/padroes/observer/Porteiro.java
@@ -0,0 +1,48 @@
+package padroes.observer;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Scanner;
+
+public class Porteiro {
+	
+	
+	private List<ObservadorChegadaAniversariante> observadores 
+		= new ArrayList<>();
+	
+	public void registrarObservador(ObservadorChegadaAniversariante observador) {
+		observadores.add(observador);
+	}
+	
+	public void monitorar() {
+		Scanner scan = new Scanner(System.in);
+
+		String valor = "";
+		
+		while (!"sair".equalsIgnoreCase(valor)) {
+			
+			System.out.println("Aniversariante chegou? ");
+			valor = scan.nextLine();
+			
+			if("sim".equalsIgnoreCase(valor)) {
+				
+				//criando o evento
+				EventoChegadaAniversariante evento = 
+						new EventoChegadaAniversariante(new Date()); 
+				
+				//notificar os observadores
+				observadores.stream().forEach(observ -> observ.chegou(evento));
+				valor = "sair";
+				
+			} else {
+				System.out.println("Alarme falso!");
+			}
+			
+		}
+		
+		scan.close();
+	}
+	
+	
+}
